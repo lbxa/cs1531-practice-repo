340b3011fba7fad528cc0de517d3b4df59d1dcdb
Lucas Barbosa <lucas.chu.barbosa@gmail.com>
2021-02-23 14:21:08 +1100

    removed left over comment

--- TW.c
+++ TW.c
@@ -164,6 +164,3 @@ void addWordToDict(char *word, Dict stopwords, Dict wfreqs) {
       if (strlen(word) > 1) DictInsert(wfreqs, word);
    }
 }
-
-// ------------------------------------------------------------------
-// STRING MANIPULATION...
